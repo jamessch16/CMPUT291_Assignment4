b216c4dfe6ef9f02b6d218ad956a6903f0eeae20
jamessch16 <jamessch16@gmail.com>
2021-03-22 00:37:16 -0600

    Add files

---
+++ A4P1.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+db_name = "./a4.db/"
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- a4.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
